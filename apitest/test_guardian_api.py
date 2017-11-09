d9beb1c3ef663c63fb1d3bbc7768bd67d59e047c
brendand <brendan.j.donegan@gmail.com>
2017-11-09 23:45:03 +0000

    Add tests for a search query and testing the page size returned is correct

--- apitest/test_guardian_api.py
+++ apitest/test_guardian_api.py
@@ -5,8 +5,23 @@ import guardianapi
 
 class GuardianAPITests(testtools.TestCase):
 
-    def test_basic_query(self):
-        # A basic test to make sure that the API returns some results
+    def test_basic_search(self):
+        # The API returns some results
         response = guardianapi.get_response()
         self.assertTrue(response.ok)
         self.assertEqual(response.status_code, 200)
+
+    def test_search_results(self):
+        #  When searching with query terms, all results contain
+        # the term
+        response = guardianapi.get_response(query='Deliveroo')
+        results = response.json()['response']['results']
+        for title in [r['webTitle'] for r in results]:
+            self.assertIn('Deliveroo', title)
+
+    def test_page_size(self):
+        # Page size reflects the number of results returned
+        response = guardianapi.get_response()
+        results = response.json()['response']['results']
+        page_size = response.json()['response']['pageSize']
+        self.assertEqual(len(results), page_size)
